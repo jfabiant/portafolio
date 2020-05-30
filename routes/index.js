d65a6c214c54b442b0d2fdf53126395c9e9cd6d0
John Fabian <john.timoteo@tecsup.edu.pe>
2020-05-30 18:54:57 -0500

    fix all

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contact');
+});
+
+router.get('/procesar_contacto', function(req, res, next) {
+  res.render('contact');
 });
 
 module.exports = router;
